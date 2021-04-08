d48d6c63d9b3c2253dba10013669b4575395cf18
Daniel Mueller <danielpetermueller@gmail.com>
2021-04-08 19:38:09 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,24 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+var foo = "foo"
+//array created
+function addElementToBeginningOfArray(chocolateBars, foo){
+  return[foo, ...chocolateBars];
+}
+//added "foo" to the beginning of the array and did not alter the original array
+function destructivelyAddElementToBeginningOfArray(chocolateBars, foo){
+  chocolateBars.unshift(foo);
+  return chocolateBars;
+}
+//added foo to the beginning of the array, however, due to the unshift element, it changed the original array
+function addElementToEndOfArray(chocolateBars,foo){
+  return[...chocolateBars,foo];
+}
+//added "foo" to the end of the array and did not alter the original array
+function destructivelyAddElementToEndOfArray(chocolateBars,foo){
+  chocolateBars.push(foo);
+  return chocolateBars;
+}
+//added "foo" to the end of the array, and due to the "push" element, it altered the original array
+function accessElementInArray(chocolateBars,index){
+  return(chocolateBars[2]);
+}
