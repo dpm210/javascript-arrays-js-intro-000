3a19f607ab8d460ee039c158141cbd749d1d29da
Daniel Mueller <danielpetermueller@gmail.com>
2021-04-08 19:57:54 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -38,6 +38,5 @@ function destructivelyRemoveElementFromEndOfArray(chocolateBars){
 }
 //removed element from end of the array and mutated it using pop 
 function removeElementFromEndOfArray(chocolateBars){
-  chocolateBars.slice(1, chocolateBars.length - 1);
-  return chocolateBars;
+  return chocolateBars.slice(1, chocolateBars.length - 1);
 }
